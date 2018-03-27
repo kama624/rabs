e23ba1baf048db8bb19bf1ba6cad9d1b45b80eba
kama624 <kama624@naver.com>
2018-03-27 17:13:19 +0900

    a

--- src/main/java/rabs/com/app/controller/HomeController.java
+++ src/main/java/rabs/com/app/controller/HomeController.java
@@ -4,12 +4,16 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import rabs.com.app.service.HomeService;
 
 /**
  * Handles requests for the application home page.
@@ -19,6 +23,9 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+    @Resource(name="homeService")
+    private HomeService homeService;
+    
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
